afca592c930663f8a1e4c69d3a0aec8d634c9f4e
Andrieu Quentin <quentinandrieumail@gmail.com>
2018-06-16 11:04:20 +1200

    Remove gatsby for pure html

--- gatsby-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Quentin Andrieu',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
--- src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-    <footer>
-        Copyright Quentin Andrieu 2018
-    </footer>
-);
-
-export default Footer;
